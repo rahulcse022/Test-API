b356a8dc9b8c26b00ea5f12a4c3b1aa26449bdc7
Rahul Saini <53773271+rahulcse022@users.noreply.github.com>
2023-02-12 19:30:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-# Using flask to make an api
-# import necessary libraries and functions
 from flask import Flask, jsonify, request
 
 # creating a Flask app
 app = Flask(__name__)
 
-# on the terminal type: curl http://127.0.0.1:5000/
-# returns hello world when we use GET.
-# returns the data that we send when we use POST.
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	if(request.method == 'GET'):
+    if (request.method == 'GET'):
 
-		data = "hello world"
-		return jsonify({'data': data})
+        data = {
+            "data1": "D1",
+            "data2": "D2",
+            "data3": "D3",
+            "data4": 1,
+            "data5": 2,
+            "data6": 3
+        }
+        return jsonify(data)
 
 
-# A simple function to calculate the square of a number
-# the number to be squared is sent in the URL when we use GET
-# on the terminal type: curl http://127.0.0.1:5000 / home / 10
-# this returns 100 (square of 10)
-@app.route('/home/<int:num>', methods = ['GET'])
+@app.route('/home/<int:num>', methods=['GET'])
 def disp(num):
 
-	return jsonify({'data': num**2})
+    return jsonify({'data': num**2})
 
 
 # driver function
 if __name__ == '__main__':
-	app.run(debug = True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
